76c405fdfecfec2ee8bc490be32725f5993e97d3
CarlosJCD <carlos.calderon.d@icloud.com>
2023-04-18 21:03:50 -0600

    Video #107 - Seccion #10 - Operaciones con Strings

--- Proyecto_5_introduccion_a_javascript/js/strings.js
+++ Proyecto_5_introduccion_a_javascript/js/strings.js
@@ -7,3 +7,22 @@ let stringComoObjeto = new String("String como objeto!!") // Object
 console.log(CURP)
 console.log(diaDeLaSemanaActual)
 console.log(stringComoObjeto)
+
+
+/* Metodos de strings*/
+
+//length (length es un atributo!!!!!!)
+console.log("Dia de la semana: " + diaDeLaSemanaActual + ", Numero de caracteres de la cadena: " + diaDeLaSemanaActual.length)
+
+//indexOf (indexamiento empezando en 0)
+let randomString = "xu5355aabhfh4bcazhks";
+
+console.log(`String: ${randomString}, Posicion de la letra 'b': ${randomString.indexOf('b')}`)
+
+//includes
+
+const diasDeTrabajo = "Lunes Martes Miercoles Jueves Viernes Sabado"
+
+console.log(`Dias de Trabajo: ${diasDeTrabajo}`)
+console.log(`¿Trabajo el Lunes?: ${diasDeTrabajo.includes('Lunes')}`)
+console.log(`¿Trabajo el Domingo?: ${diasDeTrabajo.includes('Domingo')}`)
