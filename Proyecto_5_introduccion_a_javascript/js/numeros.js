6718208677a85fe277e8e9110598c18b41890529
CarlosJCD <carlos.calderon.d@icloud.com>
2023-04-18 21:18:56 -0600

    Video #108 - Seccion #10 - Numeros en JavaScript

---
+++ Proyecto_5_introduccion_a_javascript/js/numeros.js
@@ -0,0 +1,11 @@
+//Numeros en JavaScript
+
+const cantidadDeLetrasDelAbecedario = 29;
+
+let numeroDeVideosVistos = 108;
+
+let numeroGrande = 10000000000000000000000000000000n;
+
+console.log(` ${cantidadDeLetrasDelAbecedario}, tipo: ${typeof (cantidadDeLetrasDelAbecedario)}`);
+console.log(`${numeroDeVideosVistos}, tipo: ${typeof (numeroDeVideosVistos)}`)
+console.log(`${numeroGrande}, tipo: ${typeof (numeroGrande)}`)
