7429d964b2f50e4bef4e82c76659d58e83f616b6
CarlosJCD <carlos.calderon.d@icloud.com>
2023-04-19 14:30:05 -0600

    Video #118 - Seccion #10 - Arrays en JavaScript

---
+++ Proyecto_5_introduccion_a_javascript/js/arrays.js
@@ -0,0 +1,33 @@
+//Arrays o Arreglos
+
+
+// Al igual que con los objetos, const no impide la modificacion del arreglo actual.
+const estudiantesMentoria = ["Carlos", "Carlos", "Fernando", "Cristian", "Kevin", "Oswaldo", "Alfonso"];
+console.log(estudiantesMentoria);
+
+estudiantesMentoria[0] = "Javier";
+console.table(estudiantesMentoria);
+
+// estudiantesMentoria = ["Otro", "arreglo", "distinto"]; -- TypeError, attempted to reasign readonly property
+
+// Los arrays pueden contener multiples tipos de datos a la vez, inclusive otros arreglos
+
+const multiplesDatos = ["Hola", 50, false, null, undefined, ["Arreglo en arreglo!!"], { texto: "objeto en arreglo!!" }];
+console.log(multiplesDatos)
+
+// En los arreglos se puede acceder a sus elemenntos a traves de sus indices:
+
+let primerElemento = estudiantesMentoria[0];
+let segundoElemento = estudiantesMentoria[1];
+let tercerElemento = estudiantesMentoria[2];
+console.log(primerElemento);
+console.log(segundoElemento);
+console.log(tercerElemento);
+
+// la propiedad length tambien existe en los arreglos.
+let numElementosDelArreglo = multiplesDatos.length
+
+//forEach --  recorre todo el arreglo elemento por elemento 
+estudiantesMentoria.forEach(function (estudiante) {
+    console.log(`Estudiante: ${estudiante}`)
+})
