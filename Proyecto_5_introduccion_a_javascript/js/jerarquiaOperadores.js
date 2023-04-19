d9ba06ab1cef9ac99a3b91be31547791a5e3abe3
CarlosJCD <carlos.calderon.d@icloud.com>
2023-04-18 21:58:50 -0600

    Video #110 - Seccion #10 - Operadores

---
+++ Proyecto_5_introduccion_a_javascript/js/jerarquiaOperadores.js
@@ -0,0 +1,25 @@
+// Jerarquia operaciones
+
+/*
+Sigue la jararquia tradicional de la aritmetica:
+
+De izquierda a derecha
+
+Primero Parentesis
+Luego Exponenciales y Raices
+Despues Multiplicaciones y Divisiones
+Finalmente Sumas y restas
+
+*/
+
+/*
+Operadores javascript:
+
+Utiliza los mismos operadores que java, a excepcion de:
+
+=== y !=== :igualdad de valor y tipo
+10 == '10' --> true
+10 === '10' --> false
+15 === 15 --> true
+
+*/
