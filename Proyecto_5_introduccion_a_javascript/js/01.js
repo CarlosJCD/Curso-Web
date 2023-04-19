0fb4f46f32eed8b5c64e6294d1fe47503f2ec32c
CarlosJCD <carlos.calderon.d@icloud.com>
2023-04-18 20:18:02 -0600

    Video #104 - Seccion #10 - Variables con palabra reservada 'var'

---
+++ Proyecto_5_introduccion_a_javascript/js/01.js
@@ -0,0 +1,17 @@
+// Variables
+
+var nombre = "Carlos Calderon";
+
+var disponible; // Creando la referencia de la variable
+
+disponible = true; // Reasignando un valor a la referencia de la variable
+
+
+// Estandares de Codificacion (nombramiento de variables):
+/*
+camelCase
+PascalCase
+snake_case
+
+*/
+
