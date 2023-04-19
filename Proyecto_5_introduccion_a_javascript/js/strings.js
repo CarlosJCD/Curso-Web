f2577a67444ceffdd7176e9e02870c51799f6834
CarlosJCD <carlos.calderon.d@icloud.com>
2023-04-18 20:49:50 -0600

    Video #106 - Seccion #10 - Strings en JavaScript

---
+++ Proyecto_5_introduccion_a_javascript/js/strings.js
@@ -0,0 +1,9 @@
+// maneras de crear strings
+
+const CURP = "AUGM770826MCSGRR68"; // String
+let diaDeLaSemanaActual = 'Martes'; // String
+let stringComoObjeto = new String("String como objeto!!") // Object
+
+console.log(CURP)
+console.log(diaDeLaSemanaActual)
+console.log(stringComoObjeto)
