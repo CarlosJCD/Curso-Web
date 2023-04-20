6cdf42504a36b1fae0fcbf7a13cd51d84f287d97
CarlosJCD <carlos.calderon.d@icloud.com>
2023-04-19 20:17:47 -0600

    Video #127 - Seccion #10 - Arrow Function

---
+++ Proyecto_5_introduccion_a_javascript/js/arrowFunctions.js
@@ -0,0 +1,11 @@
+// Arrow Functions -- Funciones flecha
+
+const saludos = (nombre) => {
+    console.log(`Hola ${nombre}!`)
+}
+
+saludos("Carlos")
+
+const sumaDosNumeros = (num1, num2) => num1 + num2;
+
+console.log(sumaDosNumeros(10, 13))
