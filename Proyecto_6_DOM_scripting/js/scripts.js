dbd34ccbd94a03ed7d9a45bc1a536137561a0047
CarlosJCD <carlos.calderon.d@icloud.com>
2023-04-21 14:37:16 -0600

    Video #144 - Seccion #11 - querySelector

---
+++ Proyecto_6_DOM_scripting/js/scripts.js
@@ -0,0 +1,22 @@
+
+
+/*
+Seleccionar contenido
+
+1.- querySelector
+
+2.- querySelectorAll
+
+3.- getElementById
+
+*/
+
+// querySelector
+
+const heading = document.querySelector('.header__texto h2');
+console.log(heading);
+
+// querySelectorAll
+
+
+// getElementById
