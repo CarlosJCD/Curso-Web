0fa3f53a58f4be6ff8d27bdbfe9149f2369f2d0a
CarlosJCD <carlos.calderon.d@icloud.com>
2023-04-21 17:23:31 -0600

    Video #146 - Seccion #11 - Creando HTML con createElement

--- Proyecto_6_DOM_scripting/js/scripts.js
+++ Proyecto_6_DOM_scripting/js/scripts.js
@@ -7,8 +7,6 @@ Seleccionar contenido
 
 2.- querySelectorAll
 
-3.- getElementById
-
 */
 
 // querySelector -- Retorna el 1er elemento que encuentre o null si no encuentra ninguno
@@ -19,10 +17,16 @@ console.log(heading);
 
 // querySelectorAll -- Retorna todos los elementos que coincidan 
 const enlaces = document.querySelectorAll('.navegacion a');
-console.log(enlaces[0]);
-enlaces[0].textContent = 'Sample text';
-enlaces[0].href = 'http://google.com';
 
-// getElementById
 
-const heading2 = document.getElementById();
+const nuevoEnlace = document.createElement("A");
+
+nuevoEnlace.href = "nuevo-enlace.hmtl";
+
+nuevoEnlace.textContent = "Nuevo Enlace";
+
+nuevoEnlace.classList.add("navegacion__enlace");
+
+const barraDeNavegacion = document.querySelector('.navegacion');
+
+barraDeNavegacion.appendChild(nuevoEnlace);
