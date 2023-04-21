511a33e1a9a994a333725dbc00377fda4f981264
CarlosJCD <carlos.calderon.d@icloud.com>
2023-04-20 19:19:46 -0600

    Video #142 - Secion #10 - 2 consultas async await

--- Proyecto_5_introduccion_a_javascript/js/async.js
+++ Proyecto_5_introduccion_a_javascript/js/async.js
@@ -5,21 +5,32 @@ function descargarNuevosClientes() {
         console.log('Descargando clientes... espere...');
 
         setTimeout(() => {
-            resolve('Los Clientes fueron Descargados');
+            resolve('Los clientes fueron Descargados');
         }, 5000);
+    });
+}
+
+function descargarUltimosPedidos() {
+    return new Promise(resolve => {
+        console.log('Descargando pedidos... espere...');
+
+        setTimeout(() => {
+            resolve('Los pedidos fueron Descargados');
+        }, 3000);
     });
 }
 
 async function app() {
     try {
-        const resultado = await descargarNuevosClientes();
-        console.log('Este codigo si se bloquea');
-        console.log(resultado);
+        // const clientes = await descargarNuevosClientes();
+        // const pedidos = await descargarUltimosPedidos();
+        // console.log(clientes);
+        // console.log(pedidos)
+        const resultado = await Promise.all([descargarNuevosClientes(), descargarUltimosPedidos()]);
+        resultado.forEach(element => console.log(element))
     } catch (error) {
         console.log(error)
     }
 }
 
 app();
-
-console.log('Este codigo no se bloquea');
