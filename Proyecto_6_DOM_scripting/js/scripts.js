e08e0845fbe47f2179a5baea3f4a926d206f7a65
CarlosJCD <carlos.calderon.d@icloud.com>
2023-04-21 17:02:17 -0600

    Video #145 - Seccion #10 - querySelectorAll

--- Proyecto_6_DOM_scripting/js/scripts.js
+++ Proyecto_6_DOM_scripting/js/scripts.js
@@ -11,12 +11,18 @@ Seleccionar contenido
 
 */
 
-// querySelector
+// querySelector -- Retorna el 1er elemento que encuentre o null si no encuentra ninguno
 
 const heading = document.querySelector('.header__texto h2');
+heading.textContent = "Nuevo heading";
 console.log(heading);
 
-// querySelectorAll
-
+// querySelectorAll -- Retorna todos los elementos que coincidan 
+const enlaces = document.querySelectorAll('.navegacion a');
+console.log(enlaces[0]);
+enlaces[0].textContent = 'Sample text';
+enlaces[0].href = 'http://google.com';
 
 // getElementById
+
+const heading2 = document.getElementById();
