a71114798cc0551599bb7859764d089be6b55e12
CarlosJCD <carlos.calderon.d@icloud.com>
2023-04-21 23:10:23 -0600

    Video #150 - Seccion #11 - Eventos Submit y Click

--- Proyecto_6_DOM_scripting/js/scripts.js
+++ Proyecto_6_DOM_scripting/js/scripts.js
@@ -11,72 +11,82 @@ Seleccionar contenido
 
 // querySelector -- Retorna el 1er elemento que encuentre o null si no encuentra ninguno
 
-const heading = document.querySelector('.header__texto h2');
-heading.textContent = "Nuevo heading";
-console.log(heading);
+// const heading = document.querySelector('.header__texto h2');
+// heading.textContent = "Nuevo heading";
+// console.log(heading);
 
-// querySelectorAll -- Retorna todos los elementos que coincidan 
-const enlaces = document.querySelectorAll('.navegacion a');
+// // querySelectorAll -- Retorna todos los elementos que coincidan 
+// const enlaces = document.querySelectorAll('.navegacion a');
 
 
-const nuevoEnlace = document.createElement("A");
+// const nuevoEnlace = document.createElement("A");
 
-nuevoEnlace.href = "nuevo-enlace.hmtl";
+// nuevoEnlace.href = "nuevo-enlace.hmtl";
 
-nuevoEnlace.textContent = "Nuevo Enlace";
+// nuevoEnlace.textContent = "Nuevo Enlace";
 
-nuevoEnlace.classList.add("navegacion__enlace");
+// nuevoEnlace.classList.add("navegacion__enlace");
 
-const barraDeNavegacion = document.querySelector('.navegacion');
+// const barraDeNavegacion = document.querySelector('.navegacion');
 
-barraDeNavegacion.appendChild(nuevoEnlace);
+// barraDeNavegacion.appendChild(nuevoEnlace);
 
-// Eventos
+// // Eventos
 
-window.addEventListener('load', function (evento) {
-    console.log(evento);
-});
+// window.addEventListener('load', function (evento) {
+//     console.log(evento);
+// });
 
-document.addEventListener('DOMContentLoaded', function (evento) {
-    console.log(evento);
-});
+// document.addEventListener('DOMContentLoaded', function (evento) {
+//     console.log(evento);
+// });
 
-window.onscroll = () => console.log('scrolling...');
+// window.onscroll = () => console.log('scrolling...');
 
 // asociar eventos a elementos de html
 
-
-
-
 // Eventos con el teclado
 
-
-// Campo de formularo: Nombre
 
 const datos = {
     nombre: "",
     email: "",
     mensaje: "",
 }
-
+// Primer elemento con id "nombre"
 const inputNombre = document.querySelector("#nombre");
+// Primer elemento con id "email"
 const inputEmail = document.querySelector("#email");
+// Primer elemento con id "mensaje"
 const inputMensaje = document.querySelector("#mensaje");
+// Primer elemento con la clase "boton--primario"
 const botonEventoEnviar = document.querySelector('.boton--primario');
+
+//validar un formulario
+const formulario = document.querySelector(".formulario");
+
+// Escribiendo en el elemento seleccionado por inputNombre
+inputNombre.addEventListener("input", actualizarAtributoDatos);
+
+// Escribiendo en el elemento seleccionado por inputEmail
+inputEmail.addEventListener("input", actualizarAtributoDatos);
+
+// Escribiendo en el elemento seleccionado por inputMensaje
+inputMensaje.addEventListener("input", actualizarAtributoDatos);
+
+// Click al elemento  seleccionado por botonEventoEnviar
+botonEventoEnviar.addEventListener('click', function (evento) {
+    evento.preventDefault()
+    console.log(datos);
+});
+
+formulario.addEventListener("submit", function (evento) {
+    evento.preventDefault();
+    console.log("enviando formulario...");
+});
 
 function actualizarAtributoDatos(evento) {
     const idCampo = evento.target.id;
     const valorCampo = evento.target.value;
     datos[idCampo] = valorCampo;
 }
-inputNombre.addEventListener("input", actualizarAtributoDatos);
-
-inputEmail.addEventListener("input", actualizarAtributoDatos);
-
-inputMensaje.addEventListener("input", actualizarAtributoDatos);
-
-
-botonEventoEnviar.addEventListener('click', function (evento) {
-    evento.preventDefault()
-    console.log(datos);
-});
