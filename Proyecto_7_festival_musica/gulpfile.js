6ab814bb692a8127050924aedc1eb3a9bd6ac74e
CarlosJCD <carlos.calderon.d@icloud.com>
2023-04-24 23:51:20 -0600

    Video #188 - Seccion #15 - Convertir a webp con gulp

--- Proyecto_7_festival_musica/gulpfile.js
+++ Proyecto_7_festival_musica/gulpfile.js
@@ -1,6 +1,7 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+const webp = require("gulp-webp")
 
 function compileCSS(callback) {
 
@@ -12,6 +13,16 @@ function compileCSS(callback) {
     callback();
 }
 
+function convertirAWebp(callback) {
+    const opciones = {
+        quality: 50
+    }
+    src("src/img/**/*.{png,jpg}")
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'));
+    callback();
+}
+
 function dev(callback) {
     watch("src/scss/**/*.scss", compileCSS);
 
@@ -20,4 +31,6 @@ function dev(callback) {
 
 exports.compileCSS = compileCSS;
 
-exports.dev = dev;
+exports.convertirAWebp = convertirAWebp;
+
+exports.dev = parallel(convertirAWebp, dev);
