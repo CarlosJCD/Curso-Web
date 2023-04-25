4c9c04a2a2c1e0bc53f46d6468efd30197005293
CarlosJCD <carlos.calderon.d@icloud.com>
2023-04-25 13:05:33 -0600

    Video #197 - Seccion #15 - Generando HTML de la galeria con JavaScript

--- Proyecto_7_festival_musica/src/js/galeria.js
+++ Proyecto_7_festival_musica/src/js/galeria.js
@@ -8,5 +8,13 @@ function main() {
 
 function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes');
-    galeria.textContent = "Huola";
+    for (let i = 1; i <= 12; i++) {
+        const imagen = document.createElement('picture');
+        imagen.innerHTML = `
+        <source srcset="build/img/thumb/${i}.avif" type="image/avif" />
+        <source srcset="build/img/thumb/${i}.webp" type="image/webp" />
+        <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen galeria ${i}" />
+        `
+        galeria.appendChild(imagen);
+    }
 }
