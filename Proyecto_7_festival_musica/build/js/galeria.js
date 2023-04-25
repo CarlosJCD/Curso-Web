21780fc7243bb47ab4eb405127af5e4bed77f05f
CarlosJCD <carlos.calderon.d@icloud.com>
2023-04-25 14:00:27 -0600

    Video #199 - Seccion #15 - Estilos a la galeria (close-up)

--- Proyecto_7_festival_musica/build/js/galeria.js
+++ Proyecto_7_festival_musica/build/js/galeria.js
@@ -14,7 +14,24 @@ function crearGaleria() {
         <source srcset="build/img/thumb/${i}.avif" type="image/avif" />
         <source srcset="build/img/thumb/${i}.webp" type="image/webp" />
         <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen galeria ${i}" />
-        `
+        `;
+        imagen.onclick = () => mostrarImagen(i);
         galeria.appendChild(imagen);
     }
+}
+
+function mostrarImagen(id) {
+
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = `
+    <source srcset="build/img/grande/${id}.avif" type="image/avif" />
+    <source srcset="build/img/grande/${id}.webp" type="image/webp" />
+    <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen galeria ${id}" />
+    `;
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+
+    const body = document.querySelector("body");
+    body.appendChild(overlay);
 }
