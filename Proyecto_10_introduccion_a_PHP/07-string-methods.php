168955bf41043a24ca28fc8a243c338a8fb84749
CarlosJCD <carlos.calderon.d@icloud.com>
2023-04-30 00:35:04 -0600

    Video #260 - Seccion #24 - Metodos de string

--- Proyecto_10_introduccion_a_PHP/07-string-methods.php
+++ Proyecto_10_introduccion_a_PHP/07-string-methods.php
@@ -1,6 +1,29 @@
 <?php include 'includes/header.php';
 
+// funciones para strings
+$lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
 
+//strlen 
+echo(strlen($lorem));
+
+// trim - eliminar espacios en blanco
+
+echo (trim($lorem));
+
+// strtoupper
+
+echo (strtoupper($lorem));
+
+// strtolower
+echo(strtolower($lorem));
+
+// str_reoplace
+
+echo str_replace("HOLAAAA", "Lorem", $lorem);
+
+// strpos -- validar la existencia de un substring
+
+echo strpos($lorem,'dolor');
 
 
 include 'includes/footer.php';
