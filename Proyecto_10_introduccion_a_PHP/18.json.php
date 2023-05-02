4f8a3d4c981194a46a1f7ceca28fc1b68f02f21d
CarlosJCD <carlos.calderon.d@icloud.com>
2023-05-01 23:17:01 -0600

    Video #274 - Seccion #24 (FIN SECCION) - JSON en PHP

--- Proyecto_10_introduccion_a_PHP/18.json.php
+++ Proyecto_10_introduccion_a_PHP/18.json.php
@@ -1,8 +1,41 @@
 <?php include 'includes/header.php';
 
+/* 
+Funciones que manejan JSON.
 
+json_encode($array) -> Transforma un array a un string en formato JSON
 
+json_decode($json) -> Convierte un string en formato JSON a un array asociativo
+*/
 
+$productos = [
+    [
+        'nombre' => 'Tablet', 
+        'precio' => 200,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Televisión 24"', 
+        'precio' => 300,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Monitor Curvo', 
+        'precio' => 400,
+        'disponible' => false
+    ]
+];
+
+echo "<pre>";
+//var_dump($productos);
+
+$json = json_encode($productos, JSON_UNESCAPED_UNICODE);
+
+$json_array = json_decode($json);
+
+//var_dump($json);
+var_dump($json_array);
+echo "</pre>";
 
 
 include 'includes/footer.php';
