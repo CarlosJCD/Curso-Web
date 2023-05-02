a7e5eae4ef075cdc0bd7d119876256d11b3ef6fc
CarlosJCD <carlos.calderon.d@icloud.com>
2023-05-01 22:12:24 -0600

    Video #263 - Seccion #24 - Arrays asociativos

--- Proyecto_10_introduccion_a_PHP/09-arrays-assoc.php
+++ Proyecto_10_introduccion_a_PHP/09-arrays-assoc.php
@@ -1,6 +1,22 @@
 <?php include 'includes/header.php';
 
+//arreglos asociativos (Hashmaps/ Diccionarios)
 
+$usuario = [
+    'nombre' => 'Carlos',
+    'edad' => '20 años',
+    'Tech Stack' =>[ 'python', 'java', 'bash CLI', 'html', 'css'. 'JS DOM Scripting'],
+    'PC'=>[
+        'tipo' => 'laptop',
+        'marca' => 'apple',
+        'modelo' => 'macbook pro',
+        'lanzamiento' => 'mid 2012',
+        'OS' => 'MacOS 10.15 Catalina'
+    ]
+    ];
 
+echo '<pre>';
+var_dump($usuario);
+echo '</pre>';
 
 include 'includes/footer.php';
