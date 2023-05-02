a16e4a576cf1c5be26c9f52b6f19d6c6530e2793
CarlosJCD <carlos.calderon.d@icloud.com>
2023-05-01 22:18:37 -0600

    Video #264 - Seccion #24 - Isset y empty

--- Proyecto_10_introduccion_a_PHP/10-isset-empty.php
+++ Proyecto_10_introduccion_a_PHP/10-isset-empty.php
@@ -1,6 +1,11 @@
 <?php include 'includes/header.php';
 
+/*
 
+empty($anyArray) -> Valida si un arreglo esta vacio, retorna true si esta vacio, false si no esta vacio
 
+isset($variable) -> Valida si la referencia "$variable" tiene un valor asignado
+
+*/
 
 include 'includes/footer.php';
