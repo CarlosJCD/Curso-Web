b115b29263b6a7622e32aaef02d9281b6dd5de2f
CarlosJCD <carlos.calderon.d@icloud.com>
2023-05-01 22:34:30 -0600

    Video #266 - Seccion #24 - Condiciones en PHP

--- Proyecto_10_introduccion_a_PHP/12-condicionales.php
+++ Proyecto_10_introduccion_a_PHP/12-condicionales.php
@@ -1,7 +1,34 @@
 <?php include 'includes/header.php';
 
+// estructuras de control condicionales: if, else, switch
+
+$variable;
+
+if(isset($variable)){
+    echo 'Asginado';
+} else{
+    echo 'no asignado';
+}
 
 
+
+switch (1E10 <=> 2.5E20) {
+    case '0':
+        echo ' Iguales!!';
+        break;
+    
+    case 1:
+        echo ' Mayor que!!';
+        break;
+    
+    case -1:
+        echo ' Menor que!!';
+        break;
+    
+    default:
+        echo " No debo de aparecer!!!";
+        break;
+}
 
 
 include 'includes/footer.php';
