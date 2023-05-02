d49b8f138f221d157670eb2b8a13da4bbb3f9dd6
CarlosJCD <carlos.calderon.d@icloud.com>
2023-05-01 23:10:08 -0600

    Video #273 - Seccion #24 - Require e Include

--- Proyecto_10_introduccion_a_PHP/17-require.php
+++ Proyecto_10_introduccion_a_PHP/17-require.php
@@ -1,8 +1,13 @@
 <?php include 'includes/header.php';
 
+/*
+include -> importa otros documentos al archivo, puede generar un warning, sin embargo no interrumpe la ejecucion
 
+require -> misma funcionalidad que include, excepto a que genera un error e interrumpe la ejecucion.
 
+*/
 
+require '16-types.php';
 
 
 include 'includes/footer.php';
