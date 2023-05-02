723dab012f00a9b829f7273e59008e70369299ac
CarlosJCD <carlos.calderon.d@icloud.com>
2023-05-01 23:01:09 -0600

    Videos #269 a #272 - Seccion #24 - Tipado y valores de retorno en PHP

--- Proyecto_10_introduccion_a_PHP/16-types.php
+++ Proyecto_10_introduccion_a_PHP/16-types.php
@@ -1,7 +1,13 @@
 <?php include 'includes/header.php';
 
+function is_odd(int $number): bool{
+    return ($number %2 === 1);
+}
 
+function main(): void{
+    var_dump(is_odd(12));
+}
 
-
+main();
 
 include 'includes/footer.php';
