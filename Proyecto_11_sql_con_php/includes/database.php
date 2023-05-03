edbb7bf21d193ff5ddbcd309a15545e0c7c00e99
CarlosJCD <carlos.calderon.d@icloud.com>
2023-05-02 21:36:13 -0600

    Video #276 - Seccion #25  - Consultas SQL desde PHP

--- Proyecto_11_sql_con_php/includes/database.php
+++ Proyecto_11_sql_con_php/includes/database.php
@@ -1,7 +1,3 @@
 <?php
 
 $db_connection = mysqli_connect(hostname: 'localhost', password: '', username: 'root', database: 'appsalon');
-
-echo '<pre>';
-var_dump($db_connection);
-echo '</pre>';
--- Proyecto_11_sql_con_php/includes/funciones.php
+++ Proyecto_11_sql_con_php/includes/funciones.php
@@ -1,1 +1,26 @@
 <?php
+function obtener_servicios()
+{
+    try {
+        require 'database.php';
+
+        $query_statement = "SELECT * FROM servicios";
+
+        $query = mysqli_query($db_connection, $query_statement);
+
+        echo '<pre>';
+        var_dump(mysqli_fetch_all($query));
+        echo '</pre>';
+    } catch (mysqli_sql_exception $e) {
+        echo '<pre>';
+        var_dump($e);
+        echo '</pre>';
+    } finally {
+        $db_closed = mysqli_close($db_connection);
+        echo '<pre>';
+        var_dump($db_closed);
+        echo '</pre>';
+    }
+}
+
+obtener_servicios();
