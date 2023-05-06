6eaaeede84ddd7053e2b8fa3c04babf9b89ab15e
CarlosJCD <carlos.calderon.d@icloud.com>
2023-05-06 00:07:55 -0600

    Video #306 - Seccion #29 (INICIO SECCION) - Plantilla header con PHP

---
+++ Proyecto_12_BIENES_RAICES/blog.php
@@ -0,0 +1,107 @@
+<?php include 'includes/templates/header.php' ?>
+
+<main class="contenedor seccion contenido-centrado">
+  <h1>Nuestro Blog</h1>
+
+  <article class="entrada-blog">
+    <div class="imagen">
+      <picture>
+        <source srcset="build/img/blog1.webp" type="image/webp" />
+        <source srcset="build/img/blog1.jpg" type="image/jpeg" />
+        <img loading="lazy" src="build/img/blog1.jpg" alt="Texto Entrada Blog" />
+      </picture>
+    </div>
+
+    <div class="texto-entrada">
+      <a href="entrada.html">
+        <h4>Terraza en el techo de tu casa</h4>
+        <p>Escrito el: <span>10/05/2023</span> por: <span>Admin</span></p>
+
+        <p>
+          Consejos para construir una terraza en el techo de tu casa con los
+          mejores materiales y ahorrando dinero
+        </p>
+      </a>
+    </div>
+  </article>
+
+  <article class="entrada-blog">
+    <div class="imagen">
+      <picture>
+        <source srcset="build/img/blog2.webp" type="image/webp" />
+        <source srcset="build/img/blog2.jpg" type="image/jpeg" />
+        <img loading="lazy" src="build/img/blog2.jpg" alt="Texto Entrada Blog" />
+      </picture>
+    </div>
+
+    <div class="texto-entrada">
+      <a href="entrada.html">
+        <h4>Guía para la decoración de tu hogar</h4>
+        <p>Escrito el: <span>17/05/2023</span> por: <span>Admin</span></p>
+
+        <p>
+          Maximiza el espacio en tu hogar con esta guia, aprende a combinar
+          muebles y colores para darle vida a tu espacio
+        </p>
+      </a>
+    </div>
+  </article>
+
+  <article class="entrada-blog">
+    <div class="imagen">
+      <picture>
+        <source srcset="build/img/blog3.webp" type="image/webp" />
+        <source srcset="build/img/blog3.jpg" type="image/jpeg" />
+        <img loading="lazy" src="build/img/blog3.jpg" alt="Texto Entrada Blog" />
+      </picture>
+    </div>
+
+    <div class="texto-entrada">
+      <a href="entrada.html">
+        <h4>Terraza en el techo de tu casa</h4>
+        <p>Escrito el: <span>05/02/2023</span> por: <span>Admin</span></p>
+
+        <p>
+          Consejos para construir una terraza en el techo de tu casa con los
+          mejores materiales y ahorrando dinero
+        </p>
+      </a>
+    </div>
+  </article>
+
+  <article class="entrada-blog">
+    <div class="imagen">
+      <picture>
+        <source srcset="build/img/blog4.webp" type="image/webp" />
+        <source srcset="build/img/blog4.jpg" type="image/jpeg" />
+        <img loading="lazy" src="build/img/blog4.jpg" alt="Texto Entrada Blog" />
+      </picture>
+    </div>
+
+    <div class="texto-entrada">
+      <a href="entrada.html">
+        <h4>Guía para la decoración de tu hogar</h4>
+        <p>Escrito el: <span>24/04/2023</span> por: <span>Admin</span></p>
+
+        <p>
+          Maximiza el espacio en tu hogar con esta guia, aprende a combinar
+          muebles y colores para darle vida a tu espacio
+        </p>
+      </a>
+    </div>
+  </article>
+</main>
+
+<footer class="footer seccion">
+  <nav class="navegacion">
+    <a href="nosotros.html">Nosotros</a>
+    <a href="anuncios.html">Anuncios</a>
+    <a href="blog.html">Blog</a>
+    <a href="contacto.html">Contacto</a>
+  </nav>
+  <p class="copyright">Todos los derechos reservados 2023 &copy;</p>
+</footer>
+<script src="build/js/bundle.min.js"></script>
+</body>
+
+</html>
