a85ba933e9695a6bc4ed4235fbe5e2701683f048
CarlosJCD <carlos.calderon.d@icloud.com>
2023-05-14 14:59:57 -0600

    Actualizacion archivo base de datos

--- Proyecto_12_BIENES_RAICES/database/bienesraices.sql
+++ Proyecto_12_BIENES_RAICES/database/bienesraices.sql
@@ -24,10 +24,10 @@ DROP TABLE IF EXISTS `propiedades`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `propiedades` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `Titulo` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Titulo` varchar(45) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `precio` decimal(10,2) DEFAULT NULL,
-  `imagen` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `descripcion` longtext COLLATE utf8_unicode_ci,
+  `imagen` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `descripcion` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `habitaciones` int DEFAULT NULL,
   `wc` int DEFAULT NULL,
   `estacionamientos` int DEFAULT NULL,
@@ -36,7 +36,7 @@ CREATE TABLE `propiedades` (
   PRIMARY KEY (`id`),
   KEY `fk_propiedades_vendedores_idx` (`vendedores_id`),
   CONSTRAINT `fk_propiedades_vendedores` FOREIGN KEY (`vendedores_id`) REFERENCES `vendedores` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +45,7 @@ CREATE TABLE `propiedades` (
 
 LOCK TABLES `propiedades` WRITE;
 /*!40000 ALTER TABLE `propiedades` DISABLE KEYS */;
-INSERT INTO `propiedades` VALUES (1,'Propiedad1',1000.00,NULL,'Propiedad de ejemplo',5,4,3,NULL,10),(2,'Hermosa Casa en la playa',1200000.00,NULL,'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat euismod neque eget elementum. Nam nec mauris purus. Sed euismod viverra orci ac laoreet. Fusce ut velit efficitur, pulvinar purus id, placerat augue. Phasellus ut sollicitudin odio. Suspendisse potenti. Suspendisse potenti. Phasellus vehicula quam malesuada nibh feugiat, in rhoncus justo consequat. Nam volutpat justo quis quam fermentum sollicitudin. Nulla scelerisque justo nec vehicula iaculis. Ut sodales molestie est, vel porttitor quam ultrices vel. Suspendisse eleifend posuere porttitor. Phasellus ligula diam, ornare quis lectus vel, lobortis aliquet nunc. Etiam ex nulla, laoreet a efficitur at, congue sit amet arcu. Morbi ut porta arcu. Praesent quis neque bibendum, blandit tellus at, euismod velit.',4,4,3,'2023-05-10',11),(3,'Hermosa Casa en la playa',1200000.00,NULL,'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat euismod neque eget elementum. Nam nec mauris purus. Sed euismod viverra orci ac laoreet. Fusce ut velit efficitur, pulvinar purus id, placerat augue. Phasellus ut sollicitudin odio. Suspendisse potenti. Suspendisse potenti. Phasellus vehicula quam malesuada nibh feugiat, in rhoncus justo consequat. Nam volutpat justo quis quam fermentum sollicitudin. Nulla scelerisque justo nec vehicula iaculis. Ut sodales molestie est, vel porttitor quam ultrices vel. Suspendisse eleifend posuere porttitor. Phasellus ligula diam, ornare quis lectus vel, lobortis aliquet nunc. Etiam ex nulla, laoreet a efficitur at, congue sit amet arcu. Morbi ut porta arcu. Praesent quis neque bibendum, blandit tellus at, euismod velit.',4,4,3,'2023-05-10',12);
+INSERT INTO `propiedades` VALUES (9,'Casa en la playa',1000000.00,'0073e213dd7eb674fc51ad2ad6d6ddd5.jpg','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet mauris efficitur, vulputate elit vel, maximus quam. Quisque aliquam, elit quis rhoncus pulvinar, nunc turpis commodo turpis, ac egestas mi justo et ipsum. Sed eu tellus ex. Suspendisse dapibus at nunc eget ornare. In pharetra lorem id tortor bibendum finibus. ',10,12,5,'2023-05-14',1);
 /*!40000 ALTER TABLE `propiedades` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -58,11 +58,11 @@ DROP TABLE IF EXISTS `vendedores`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `vendedores` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `Nombre` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `Apellido` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `numTelefono` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Nombre` varchar(45) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Apellido` varchar(45) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `numTelefono` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,7 +71,7 @@ CREATE TABLE `vendedores` (
 
 LOCK TABLES `vendedores` WRITE;
 /*!40000 ALTER TABLE `vendedores` DISABLE KEYS */;
-INSERT INTO `vendedores` VALUES (1,'Carlos','Calderon','9999038088'),(10,'Alfonso ','Martinez','9999302520'),(11,'Pedro','Fernandez','9991221426'),(12,'Pedro','Fernandez','9991221426');
+INSERT INTO `vendedores` VALUES (1,'Carlos','Calderon','9999038088');
 /*!40000 ALTER TABLE `vendedores` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,4 +88,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-09 23:24:06
+-- Dump completed on 2023-05-14 14:59:06
