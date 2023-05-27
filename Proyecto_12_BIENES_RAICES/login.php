e63a8044689f84be0247e7fc237edd69e406b65e
CarlosJCD <carlos.calderon.d@icloud.com>
2023-05-27 14:40:09 -0600

    Video #337 - Seccion #31 - Validacion sintactica login

--- Proyecto_12_BIENES_RAICES/login.php
+++ Proyecto_12_BIENES_RAICES/login.php
@@ -1,16 +1,43 @@
 <?php
+
+require "includes/config/database.php";
+
+$db = conectarDB();
+
+$errores = [];
+
+if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+    $email = mysqli_real_escape_string($db, filter_var($_POST['email'], FILTER_VALIDATE_EMAIL));
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+
+    if (!$email) {
+        $errores[] = "El email es obligatorio o no es valido";
+    }
+    if (!$password) {
+        $errores[] = "La contraseña es obligatoria";
+    }
+}
+
+
+
+
 require 'includes/funciones.php';
 añadirPlantilla('header');
 ?>
 <main class="contenedor seccion contenido-centrado">
     <h1>Iniciar Sesión</h1>
-    <form class="formulario">
+    <?php foreach ($errores as $error) : ?>
+        <div class="alerta error"><?php echo $error; ?></div>
+    <?php endforeach; ?>
+    <form method="POST" class="formulario">
         <fieldset>
             <legend>Email y Password</legend>
             <label for="email">Email</label>
-            <input type="email" id="email" placeholder="Tu email">
+            <input type="email" name="email" id="email" placeholder="Tu email">
+
             <label for="password">Password</label>
-            <input type="password" id="password" placeholder="Tu contraseña">
+            <input type="password" name="password" id="password" placeholder="Tu contraseña">
+
             <input type="submit" value="Iniciar Sesion" class="boton boton-verde">
         </fieldset>
     </form>
