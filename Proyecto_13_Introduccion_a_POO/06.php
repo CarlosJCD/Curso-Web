1e130edefa18c79c776d702c565c6aa00ef82a6c
CarlosJCD <carlos.calderon.d@icloud.com>
2023-05-29 19:05:18 -0600

    Video #352 - Seccion #32 - Interfaces

--- Proyecto_13_Introduccion_a_POO/06.php
+++ Proyecto_13_Introduccion_a_POO/06.php
@@ -1,4 +1,26 @@
 <?php include 'includes/header.php';
 
+interface ITransporte
+{
+    public function getInfo(): string;
+    public function getRuedas(): int;
+}
+class Transporte implements ITransporte
+{
+    public function __construct(protected int $ruedas, protected int $capacidad)
+    {
+    }
+
+    public function getInfo(): string
+    {
+        return "El transporte tiene " . $this->ruedas . " ruedas y una capacidad de " . $this->capacidad . " personas ";
+    }
+
+    public function getRuedas(): int
+    {
+        return $this->ruedas;
+    }
+}
+
 
 include 'includes/footer.php';
