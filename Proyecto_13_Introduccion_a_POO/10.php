bd9e3732796254a325b60aa513785957c0f2a7f3
CarlosJCD <carlos.calderon.d@icloud.com>
2023-05-29 20:47:29 -0600

    Video #360 - Seccion #32 (FIN SECCION) - Consultas con clases de PDO

--- Proyecto_13_Introduccion_a_POO/10.php
+++ Proyecto_13_Introduccion_a_POO/10.php
@@ -1,4 +1,21 @@
 <?php include 'includes/header.php';
 
+$db = new PDO('mysql:host=localhost; dbname=bienesraices', 'root', '');
+
+$query = "SELECT titulo, imagen from propiedades";
+
+$stmt = $db->prepare($query);
+
+$stmt->execute();
+
+$resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($resultado as $propiedad) :
+    echo $propiedad['titulo'];
+    echo "<br>";
+    echo $propiedad['imagen'];
+    echo "<br>";
+endforeach;
+
 
 include 'includes/footer.php';
