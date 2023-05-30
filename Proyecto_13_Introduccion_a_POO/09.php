840f8f585e6277d94385af53c55e52ccf89bd2e2
CarlosJCD <carlos.calderon.d@icloud.com>
2023-05-29 20:43:27 -0600

    Video #359 - Seccion #32 - Consultas con clases de MySQLi

--- Proyecto_13_Introduccion_a_POO/09.php
+++ Proyecto_13_Introduccion_a_POO/09.php
@@ -1,4 +1,21 @@
 <?php include 'includes/header.php';
 
+$db = new mysqli('localhost', 'root', '', 'bienesraices');
+
+$query = "SELECT titulo, imagen FROM propiedades";
+$stmt = $db->prepare($query);
+
+$stmt->execute();
+
+$stmt->bind_result($titulo, $imagen);
+
+
+while ($stmt->fetch()) {
+    echo '<pre>';
+    var_dump($imagen);
+    echo '</pre>';
+}
+
+
 
 include 'includes/footer.php';
