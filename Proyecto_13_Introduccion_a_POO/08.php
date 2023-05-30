87ee41e5b8b5d3146edcde800d978bf9bc0680cc
CarlosJCD <carlos.calderon.d@icloud.com>
2023-05-29 20:10:20 -0600

    Video #355 - Sección #32 - Namespace y paqueterias en PHP

--- Proyecto_13_Introduccion_a_POO/08.php
+++ Proyecto_13_Introduccion_a_POO/08.php
@@ -6,18 +6,19 @@ use App\Detalles;
 include 'includes/header.php';
 
 
-require 'clases/Clientes.php';
-require 'clases/Detalles.php';
+// require 'clases/Clientes.php';
+// require 'clases/Detalles.php';
+
 
 function mi_autoload($clase)
 {
-    require __DIR__ . '/clases/' . $clase . '.php';
+    $partes = explode('\\', $clase);
+    require __DIR__ . '/clases/' . $partes[1] . '.php';
 }
 
 spl_autoload_register('mi_autoload');
 
 $detalles = new Detalles();
-echo "<hr>";
 $cliente = new Clientes();
 
 
