574c5d824b88910f1957742f9c7e25e2df5fd43a
CarlosJCD <carlos.calderon.d@icloud.com>
2023-05-29 19:46:12 -0600

    Video #354 - Seccion #32 - Autocarga de una clase

--- Proyecto_13_Introduccion_a_POO/08.php
+++ Proyecto_13_Introduccion_a_POO/08.php
@@ -1,4 +1,24 @@
-<?php include 'includes/header.php';
+<?php
+
+use App\Clientes;
+use App\Detalles;
+
+include 'includes/header.php';
+
+
+require 'clases/Clientes.php';
+require 'clases/Detalles.php';
+
+function mi_autoload($clase)
+{
+    require __DIR__ . '/clases/' . $clase . '.php';
+}
+
+spl_autoload_register('mi_autoload');
+
+$detalles = new Detalles();
+echo "<hr>";
+$cliente = new Clientes();
 
 
 include 'includes/footer.php';
