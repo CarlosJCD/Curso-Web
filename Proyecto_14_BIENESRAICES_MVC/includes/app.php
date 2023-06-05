dd026af357a46423375324d9d80ca6772f1b4788
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-04 19:20:27 -0600

    Video #396 - Seccion #41 - Importando Modelo con composer

--- Proyecto_14_BIENESRAICES_MVC/includes/app.php
+++ Proyecto_14_BIENESRAICES_MVC/includes/app.php
@@ -4,7 +4,9 @@ require "funciones.php";
 require "config/database.php";
 require __DIR__ . "/../vendor/autoload.php";
 
-use App\ActiveRecord;
+
+
+use Model\ActiveRecord;
 
 
 ActiveRecord::setDB(conectarDB());
