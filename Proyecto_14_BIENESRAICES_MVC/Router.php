c30257ec66335eb40c9fc89ffe387cab79f6ac68
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-04 20:37:22 -0600

    Video #398 - Seccion #41 - Llamar y Asociar funciones a los metodos HTTP

--- Proyecto_14_BIENESRAICES_MVC/Router.php
+++ Proyecto_14_BIENESRAICES_MVC/Router.php
@@ -5,4 +5,40 @@ namespace MVC;
 
 class Router
 {
+
+    public $rutasGET = [];
+    public $rutasPOST = [];
+
+
+    public function asociarFuncionGET($ruta, $nombreFuncionAsociada)
+    {
+        $this->rutasGET[$ruta] = $nombreFuncionAsociada;
+    }
+
+    private function obtenerFuncionAsociadaARuta(String $rutaActual, String $metodo)
+    {
+        switch ($metodo) {
+            case "GET":
+                return $this->rutasGET[$rutaActual] ?? null;
+            case "POST":
+                return $this->rutasGET[$rutaActual] ?? null;
+            default:
+                return null;
+        }
+    }
+
+    public function comprobarRutas()
+    {
+        $rutaActual = $_SERVER["SCRIPT_NAME"] ?? "/";
+        $metodo = $_SERVER["REQUEST_METHOD"];
+
+        $nombreFuncionAsociada = $this->obtenerFuncionAsociadaARuta($rutaActual, $metodo);
+
+
+        if ($nombreFuncionAsociada) {
+            call_user_func($nombreFuncionAsociada, $this);
+        } else {
+            echo "Pagina no encontrada";
+        }
+    }
 }
--- Proyecto_14_BIENESRAICES_MVC/public/index.php
+++ Proyecto_14_BIENESRAICES_MVC/public/index.php
@@ -1,2 +1,8 @@
 <?php
 require_once __DIR__ . "/../includes/app.php";
+
+use MVC\Router;
+
+$router = new Router;
+
+$router->comprobarRutas();
