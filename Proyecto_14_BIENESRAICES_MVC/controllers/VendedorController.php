b838d05d94745415f4ee89e09b2997ce1c1f5160
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-05 23:48:54 -0600

    Video #411 - Seccion #41 - Creando el controlador de Vendedor

---
+++ Proyecto_14_BIENESRAICES_MVC/controllers/VendedorController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+
+class VendedorController
+{
+
+    public static function crear(Router $router)
+    {
+    }
+    public static function actualizar(Router $router)
+    {
+    }
+    public static function eliminar(Router $router)
+    {
+    }
+}
--- Proyecto_14_BIENESRAICES_MVC/public/index.php
+++ Proyecto_14_BIENESRAICES_MVC/public/index.php
@@ -3,6 +3,7 @@ require_once __DIR__ . "/../includes/app.php";
 
 use MVC\Router;
 use Controllers\PropiedadController;
+use Controllers\VendedorController;
 
 $router = new Router;
 
@@ -13,5 +14,11 @@ $router->asociarFuncionPOST('/propiedades/crear', [PropiedadController::class, '
 $router->asociarFuncionGET('/propiedades/actualizar', [PropiedadController::class, 'actualizar']);
 $router->asociarFuncionPOST('/propiedades/actualizar', [PropiedadController::class, 'actualizar']);
 $router->asociarFuncionPOST('/propiedades/eliminar', [PropiedadController::class, 'eliminar']);
+
+$router->asociarFuncionGET('/vendedores/crear', [VendedorController::class, 'crear']);
+$router->asociarFuncionPOST('/vendedores/crear', [VendedorController::class, 'crear']);
+$router->asociarFuncionGET('/vendedores/actualizar', [VendedorController::class, 'actualizar']);
+$router->asociarFuncionPOST('/vendedores/actualizar', [VendedorController::class, 'actualizar']);
+$router->asociarFuncionPOST('/vendedores/eliminar', [VendedorController::class, 'eliminar']);
 
 $router->comprobarRutas();
