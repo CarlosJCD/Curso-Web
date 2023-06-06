a25aca066c099b10ce6fd97eb2d33db9e63769af
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-06 00:04:57 -0600

    Video #412 y #413 - Seccion #41 - Adaptando crear vendedores en MVC

--- Proyecto_14_BIENESRAICES_MVC/controllers/VendedorController.php
+++ Proyecto_14_BIENESRAICES_MVC/controllers/VendedorController.php
@@ -3,12 +3,27 @@
 namespace Controllers;
 
 use MVC\Router;
+use Model\Vendedor;
 
 class VendedorController
 {
 
     public static function crear(Router $router)
     {
+        $vendedor = new Vendedor();
+        $errores = Vendedor::getErrores();
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $vendedor = new Vendedor($_POST["vendedor"]);
+            $errores = $vendedor->validar();
+            if (empty($errores)) {
+                $vendedor->registrar();
+            }
+        }
+        $router->display("vendedores/crear", [
+            "vendedor" => $vendedor,
+            "errores" => $errores
+        ]);
     }
     public static function actualizar(Router $router)
     {
