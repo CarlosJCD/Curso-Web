287a7da3707871d103c6c365afc385f90e9fae99
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-07 21:44:19 -0600

    Video #431 - Seccion #43 - Atributos y constructor del modelo "Admin"

--- Proyecto_14_BIENESRAICES_MVC/models/Admin.php
+++ Proyecto_14_BIENESRAICES_MVC/models/Admin.php
@@ -4,6 +4,22 @@ namespace Model;
 
 class Admin extends ActiveRecord
 {
-    public $username;
+
+    protected static $tabla = 'usuarios';
+    protected static $columnasDB = ['id', 'email', 'password'];
+
+    public $id;
+    public $email;
     public $password;
+
+    public function __construct($args = [])
+    {
+        $this->id = $args["id"] ?? null;
+        $this->email = $args["email"] ?? "";
+        $this->password = $args["password"] ?? "";
+    }
+
+    public function validar()
+    {
+    }
 }
