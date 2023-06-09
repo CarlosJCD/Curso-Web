a3461fa39821ed69aaa8c28ec7bda4ca5e7b475d
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-08 19:11:45 -0600

    Video #449 - Seccion #46 - Creando formulario de autenticacion

--- Proyecto_15_APPSALON/views/auth/login.php
+++ Proyecto_15_APPSALON/views/auth/login.php
@@ -1,2 +1,19 @@
 <h1 class="nombre-pagina">Iniciar Sesion</h1>
 <p class="descripcion-pagina">Inicia sesion con tus datos</p>
+
+<form action="/" class="formulario" method="POST">
+    <div class="campo">
+        <label for="email">Email</label>
+        <input type="email" id="email" placeholder="Tu Email" name="email" />
+    </div>
+    <div class="campo">
+        <label for="password">Contraseña</label>
+        <input type="password" id="password" placeholder="Tu contraseña" name="password" />
+    </div>
+    <input type="submit" class="boton" value="Iniciar Sesion">
+</form>
+
+<div class="acciones">
+    <a href="/crearCuenta">Crear cuenta</a>
+    <a href="/olvidaContraseña">Olvide mi contraseña</a>
+</div>
