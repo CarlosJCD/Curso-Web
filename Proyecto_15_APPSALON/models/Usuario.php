cde1a3cff7f75ce13591792a729ac18a94cf3b12
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-09 10:33:42 -0600

    Video #469 - Seccion #48 - Generar un token del usuario

--- Proyecto_15_APPSALON/models/Usuario.php
+++ Proyecto_15_APPSALON/models/Usuario.php
@@ -43,6 +43,11 @@ class Usuario extends ActiveRecord
         $this->password = password_hash($this->password, PASSWORD_BCRYPT);
     }
 
+    public function crearToken()
+    {
+        $this->token = uniqid();
+    }
+
     private function camposVacios()
     {
         $flag = false;
