27ebda49ad43f714db9047eb0114a0e5dca39de5
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-11 22:45:59 -0600

    Video #487 - Seccion #52 - Creacion contenedores index citas

--- Proyecto_15_APPSALON/views/cita/index.php
+++ Proyecto_15_APPSALON/views/cita/index.php
@@ -1,2 +1,20 @@
 <h1 class="nombre-pagina">Crear Nueva Citas</h1>
-<p class="descripcion-pagina">Elige tus servicios a continuacion</p>
+<p class="descripcion-pagina">Elige tus servicios y coloca tus datos</p>
+
+<div id="app">
+    <div id="paso-1" class="seccion">
+        <h2>Servicios </h2>
+        <p>Elige tus servicios a continuacion</p>
+        <div id="servicios" class="listado-servicios"></div>
+    </div>
+    <div id="paso-2" class="seccion">
+        <h2>Tus datos y cita </h2>
+        <p>Coloca tus datos y fecha de cita</p>
+
+    </div>
+    <div id="paso-3" class="seccion">
+        <h2>Resumen</h2>
+        <p>Verifica que la informacion sea correcta</p>
+
+    </div>
+</div>
