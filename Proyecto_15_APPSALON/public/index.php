d37c08f565f3c1c4439ca95b84146a5f5f642c1a
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-12 13:55:46 -0600

    Video #498 - Seccion #53 (INICIO) - APIController y clase modelo Servicio

--- Proyecto_15_APPSALON/public/index.php
+++ Proyecto_15_APPSALON/public/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\APIController;
 use Controllers\CitaController;
 use Controllers\LoginController;
 use MVC\Router;
@@ -28,6 +29,8 @@ $router->post('/cambiarContraseña', [LoginController::class, 'cambiarContraseñ
 $router->get('/mensaje', [LoginController::class, 'mensaje'], false);
 
 $router->get('/cita', [CitaController::class, 'index'], false);
+
+$router->get('/api/servicios', [APIController::class, 'index'], false);
 
 
 
