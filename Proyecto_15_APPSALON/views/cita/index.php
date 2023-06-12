355657ab2127efd2e18b8b33598d9d4187d7b977
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-12 09:49:12 -0600

    Video #489 - Seccion #52 - Creacion y estilos a los tabs de "cita"

--- Proyecto_15_APPSALON/views/cita/index.php
+++ Proyecto_15_APPSALON/views/cita/index.php
@@ -2,6 +2,11 @@
 <p class="descripcion-pagina">Elige tus servicios y coloca tus datos</p>
 
 <div id="app">
+    <nav class="tabs">
+        <button type="button" data-paso="1">Servicios</button>
+        <button type="button" data-paso="2">Informacion de la cita</button>
+        <button type="button" data-paso="3">Resumen</button>
+    </nav>
     <div id="paso-1" class="seccion">
         <h2>Servicios </h2>
         <p class="text-center">Elige tus servicios a continuacion</p>
