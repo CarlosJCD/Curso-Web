2341be8002630aca6fb2dcf41a022cbc4e1a0786
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-13 23:21:11 -0600

    Video #527 - Seccion #54 (FIN) - Validando acceso a las rutas

--- Proyecto_15_APPSALON/controllers/CitaController.php
+++ Proyecto_15_APPSALON/controllers/CitaController.php
@@ -9,6 +9,7 @@ class CitaController
     public static function index(Router $router)
     {
         session_start();
+        isAuth();
         $router->render("cita/index", [
             'id' => $_SESSION['id'],
             'nombre' => $_SESSION['nombre']
