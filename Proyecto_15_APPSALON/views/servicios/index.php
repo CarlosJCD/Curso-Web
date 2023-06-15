b75a6c6ef80980ee23859546d02e729c752e6e25
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-15 00:13:09 -0600

    Video #550 - Seccion #56 - Añadiendo botones de actualizar y eliminar servicio

--- Proyecto_15_APPSALON/views/servicios/index.php
+++ Proyecto_15_APPSALON/views/servicios/index.php
@@ -9,6 +9,13 @@ include_once __DIR__ . "/../templates/barra.php";
         <li>
             <p>Nombre: <span><?php echo $servicio->nombre ?></span></p>
             <p>Precio: <span><?php echo $servicio->precio ?></span></p>
+            <div class="acciones">
+                <a href="/servicios/actualizar?id=<?php echo $servicio->id; ?>" class="boton">Actualizar</a>
+                <form action="/servicios/eliminar" method="POST">
+                    <input type="hidden" value="<?php echo $servicio->id; ?>">
+                    <input type="submit" class="boton-eliminar" value="Eliminar Servicio">
+                </form>
+            </div>
         </li>
     <?php } ?>
 </ul>
