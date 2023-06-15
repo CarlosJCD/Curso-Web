8d18687646dca6615f4791b7b88915f1cc52b9f5
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-14 23:43:39 -0600

    Video #547 - Seccion #56 - Formulario para crear y actualizar servicios

--- Proyecto_15_APPSALON/views/servicios/crear.php
+++ Proyecto_15_APPSALON/views/servicios/crear.php
@@ -3,3 +3,8 @@
 include_once __DIR__ . "/../templates/barra.php";
 ?>
 <p class="descripcion-pagina">Llena los siguientes campos para registrar un nuevo servicio.</p>
+
+<form action="/servicios/crear" method="POST" class="formulario">
+    <?php include_once __DIR__ . "/formulario.php"; ?>
+    <input type="submit" class="boton" value="Registrar Servicio">
+</form>
--- Proyecto_15_APPSALON/views/servicios/formulario.php
+++ Proyecto_15_APPSALON/views/servicios/formulario.php
@@ -1,4 +1,9 @@
-<form method="POST">
-    <div></div>
+<div class="campo">
+    <label for="nombre">Nombre:</label>
+    <input type="text" id="nombre" name="nombre" placeholder="Nombre del servicio" value="" <?php s($servicio->nombre ?? '') ?>">
+</div>
 
-</form>
+<div class="campo">
+    <label for="precio">Precio:</label>
+    <input type="text" id="precio" name="precio" placeholder="Precio del servicio" value="<?php s($servicio->precio ?? '') ?>">
+</div>
