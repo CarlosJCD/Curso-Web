f7c64cdcad986b1558d5cf5136ccc700b0a4e57a
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-15 21:48:32 -0600

    Video #565 - Seccion #60 (FIN) - Routing LoginController

---
+++ Proyecto_16_UpTask/controllers/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Controllers;
+
+class LoginController
+{
+    public static function login()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        }
+    }
+
+    public static function logout()
+    {
+    }
+
+    public static function crearCuenta()
+    {
+    }
+
+    public static function mensajeConfirmarCuenta()
+    {
+    }
+
+    public static function confirmarCuenta()
+    {
+    }
+
+    public static function olvidaPassword()
+    {
+    }
+
+    public static function restablecePassword()
+    {
+    }
+}
--- Proyecto_16_UpTask/public/index.php
+++ Proyecto_16_UpTask/public/index.php
@@ -1,9 +1,28 @@
-<?php 
+<?php
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
+
 $router = new Router();
+
+$router->get("/", [LoginController::class, "login"]);
+$router->post("/", [LoginController::class, "login"]);
+
+$router->get("/logout", [LoginController::class, "logout"]);
+
+$router->get("/crearCuenta", [LoginController::class, "crearCuenta"]);
+$router->post("/crearCuenta", [LoginController::class, "crearCuenta"]);
+
+$router->get("/mensajeConfirmarCuenta", [LoginController::class, "mensajeConfirmarCuenta"]);
+$router->get("/confirmarCuenta", [LoginController::class, "confirmarCuenta"]);
+
+$router->get("/olvidaPassword", [LoginController::class, "olvidaPassword"]);
+$router->post("/olvidaPassword", [LoginController::class, "olvidaPassword"]);
+
+$router->get("/restablecePassword", [LoginController::class, "restablecePassword"]);
+$router->post("/restablecePassword", [LoginController::class, "restablecePassword"]);
 
 
 
