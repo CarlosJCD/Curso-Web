a0ab01049a1c942334807eb7d783a043b4234a1b
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-19 16:17:03 -0600

    Correcciones de las rutas del Email y reubicacion del archivo .env

--- Proyecto_16_UpTask/classes/Email.php
+++ Proyecto_16_UpTask/classes/Email.php
@@ -37,7 +37,7 @@ class Email
 
         $contenido = '<html>';
         $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has Creado tu cuenta en UpTask, solo debes confirmarla en el siguiente enlace</p>";
-        $contenido .= "<p>Presiona aquí: <a href='http://" . $_ENV['VHOST_NAME'] . " /confirmarCuenta?token=" . $this->token . "'>Confirmar Cuenta</a></p>";
+        $contenido .= "<p>Presiona aquí: <a href='http://" . $_ENV['VHOST_NAME'] . "/confirmarCuenta?token=" . $this->token . "'>Confirmar Cuenta</a></p>";
         $contenido .= "<p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>";
         $contenido .= '</html>';
 
