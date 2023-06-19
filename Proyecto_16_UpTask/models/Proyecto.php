32977045a0b3579a1bec47ea6287f3d537a4e933
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-19 16:32:16 -0600

    Video #607 - Seccion #67
    Creando el modelo de proyectos

---
+++ Proyecto_16_UpTask/models/Proyecto.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Model;
+
+
+#[\AllowDynamicProperties]
+class Proyecto extends ActiveRecord
+{
+    protected static $tabla = 'proyectos';
+    protected static $columnasDB = ['id', 'proyecto', 'url', 'propietarioId'];
+
+    public function __construct($args = [])
+    {
+        $this->id = $args['id'] ?? null;
+        $this->proyecto = $args['proyecto'] ?? '';
+        $this->url = $args['url'] ?? '';
+        $this->propietarioId = $args['propietarioId'] ?? '';
+    }
+}
