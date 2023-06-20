e480c051d5b540245276940fb358264b8e25923c
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-20 13:48:32 -0600

    Video #616 - Seccion #69
    Creando a traves de JS un formulario para crear una nueva tarea 
    Añadiendo el formulario a la pagina web por medio de JS

--- Proyecto_16_UpTask/src/js/tareas.js
+++ Proyecto_16_UpTask/src/js/tareas.js
@@ -3,6 +3,20 @@
     nuevaTareaBtn.addEventListener('click', mostrarFormulario);
 
     function mostrarFormulario() {
-        console.log('huola')
+        const modal = document.createElement('DIV');
+        modal.classList.add('modal');
+        modal.innerHTML = `
+        <form class="formulario nueva-tarea">
+            <legend>Añade una nueva tarea</legend>
+            <div class="campo">
+                <label for="tarea">Tarea</label>
+                <input type="text" name="tarea" placeholder="Añadir tarea al proyecto actual" id="tarea" />
+            </div>
+            <div class="opciones">
+                <input type="sumbit" class="submit-nueva-tarea" value="Añadir Tarea" />
+                <button type="button" class="cerrar-modal">Cancelar</button>
+            </div>
+        </form>`;
+        document.querySelector('body').appendChild(modal);
     }
 })();
