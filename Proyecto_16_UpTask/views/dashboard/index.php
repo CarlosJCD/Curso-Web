940a551139cae5e3e6d2fd36e9e4e76050cca204
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-19 18:44:04 -0600

    Video #612 - Seccion #68
    Desplegando proyectos del usuario
    Desplegando mensajes en caso de no tener ningun proyecto creado

--- Proyecto_16_UpTask/views/dashboard/index.php
+++ Proyecto_16_UpTask/views/dashboard/index.php
@@ -1,4 +1,17 @@
-<?php include_once __DIR__ . "/header-dashboard.php"; ?>
+<?php include_once __DIR__ . "/header-dashboard.php";
+
+
+if (count($proyectos) === 0) { ?>
+    <p class="no-proyectos">No hay proyectos aun. <a href="/crearProyecto">Comienza creando uno</a></p>
+<?php } else { ?>
+    <ul class="listado-proyectos">
+        <?php foreach ($proyectos as $proyecto) { ?>
+            <li class="proyecto">
+                <a href="/proyecto?url=<?php echo $proyecto->url ?>"><?php echo $proyecto->proyecto ?></a>
+            </li>
+        <?php } ?>
+    </ul>
+<?php } ?>
 
 
 
