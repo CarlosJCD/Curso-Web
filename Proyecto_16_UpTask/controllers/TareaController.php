53cf6eacb5cce24e5bdd7f305f645793fc36013f
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-20 17:09:32 -0600

    Video #625 - Seccion #70
    Routeo del CRUD de tareas con los metodos de TareaController

---
+++ Proyecto_16_UpTask/controllers/TareaController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Controllers;
+
+class TareaController
+{
+    public static function index()
+    {
+    }
+    public static function crear()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        }
+    }
+    public static function actualizar()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        }
+    }
+    public static function eliminar()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        }
+    }
+}
