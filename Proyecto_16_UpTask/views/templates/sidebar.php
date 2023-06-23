570607d5aa420e626f33aebb4a6697574f8d7fca
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-22 20:00:45 -0600

    Video #655 - Seccion #76
    Añadiendo un boton para ocultar el menu mobile

--- Proyecto_16_UpTask/views/templates/sidebar.php
+++ Proyecto_16_UpTask/views/templates/sidebar.php
@@ -1,5 +1,11 @@
 <aside class="sidebar">
-    <h2>UpTask</h2>
+    <div class="contenedor-sidebar">
+        <h2>UpTask</h2>
+
+        <div class="cerrar-menu">
+            <img id='cerrar-menu' src="build/img/cerrar.svg" alt="imagen cerrar menu mobile">
+        </div>
+    </div>
     <nav class="sidebar-nav">
         <a <?php echo ($titulo == 'Proyectos') ? 'class = "activo"' : ''; ?> href="/dashboard">Proyectos</a>
         <a <?php echo ($titulo == 'Crear proyecto') ? 'class = "activo"' : ''; ?> href="/crearProyecto">Crear proyecto</a>
