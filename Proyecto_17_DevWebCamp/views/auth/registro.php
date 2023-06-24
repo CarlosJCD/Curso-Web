8f7ae3ddf3fb9423a778925d8b3dbecf6bc98010
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-23 22:58:20 -0600

    Video #678 - Seccion #80
    Creando la vista de Crear Cuenta

---
+++ Proyecto_17_DevWebCamp/views/auth/registro.php
@@ -0,0 +1,36 @@
+<main class="auth">
+    <h2 class="auth__heading"><?php echo $titulo ?></h2>
+    <p class="auth__texto">Regístrate en DevWebCamp</p>
+
+    <form class="formulario">
+        <div class="formulario__campo">
+            <label for="nombre" class="formulario__label">Nombre</label>
+            <input type="text" class="formulario__input" placeholder="Tu Nombre" id="nombre" name="nombre">
+        </div>
+        <div class="formulario__campo">
+            <label for="apellido" class="formulario__label">Apellido</label>
+            <input type="text" class="formulario__input" placeholder="Tu Apellido" id="apellido" name="apellido">
+        </div>
+
+        <div class="formulario__campo">
+            <label for="email" class="formulario__label">Email</label>
+            <input type="email" class="formulario__input" placeholder="Tu Email" id="email" name="email">
+        </div>
+
+        <div class="formulario__campo">
+            <label for="password" class="formulario__label">Password</label>
+            <input type="password" class="formulario__input" placeholder="Tu Password" id="password" name="password">
+        </div>
+
+        <div class="formulario__campo">
+            <label for="password2" class="formulario__label">Repetir Password</label>
+            <input type="password" class="formulario__input" placeholder="Repite Tu Password" id="password2" name="password2">
+        </div>
+
+        <input type="submit" class="formulario__submit" value="Crear Cuenta">
+    </form>
+    <div class="acciones">
+        <a href="/login" class="acciones__enlace">¿Ya tienes cuenta? Inicia Sesió</a>
+        <a href="/olvide" class="acciones__enlace">¿Olvidaste tu Password?</a>
+    </div>
+</main>
