55baba594efcf5c1c598c304637dcd7f753eb4f5
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-24 22:13:20 -0600

    Video #692 - Seccion #83
    Creando la vista para reestablecer la contraseña

--- Proyecto_17_DevWebCamp/controllers/AuthController.php
+++ Proyecto_17_DevWebCamp/controllers/AuthController.php
@@ -194,7 +194,7 @@ class AuthController
 
                 // Redireccionar
                 if ($resultado) {
-                    header('Location: /');
+                    header('Location: /login');
                 }
             }
         }
--- Proyecto_17_DevWebCamp/views/auth/olvide.php
+++ Proyecto_17_DevWebCamp/views/auth/olvide.php
@@ -2,7 +2,11 @@
     <h2 class="auth__heading"><?php echo $titulo ?></h2>
     <p class="auth__texto">Recupera tu acceso a DevWebCamp</p>
 
-    <form class="formulario">
+    <?php
+    require_once __DIR__ . '/../templates/alertas.php';
+    ?>
+
+    <form method="POST" action="/olvide" class="formulario">
         <div class="formulario__campo">
             <label for="email" class="formulario__label">Email</label>
             <input type="email" class="formulario__input" placeholder="Tu Email" id="email" name="email">
