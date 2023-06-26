12ea20e0816da7ea556aa539da9e30a215766bee
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-26 17:55:48 -0600

    Video #696 - Seccion #84
    - Creacion iconos y enlaces de navegacion del dashboard

---
+++ Proyecto_17_DevWebCamp/views/templates/admin-sidebar.php
@@ -0,0 +1,39 @@
+<aside class="dashboard__sidebar">
+    <nav class="dashboard__menu">
+        <a href="/admin/dashboard" class="dashboard__enlace">
+            <i class="fa-solid fa-house"></i>
+            <span class="dashboard__menu-texto">
+                Inicio
+            </span>
+        </a>
+
+        <a href="/admin/ponentes" class="dashboard__enlace">
+            <i class="fa-solid fa-user-tie"></i>
+            <span class="dashboard__menu-texto">
+                Ponentes
+            </span>
+        </a>
+
+        <a href="/admin/eventos" class="dashboard__enlace">
+            <i class="fa-regular fa-calendar-days"></i>
+            <span class="dashboard__menu-texto">
+                Eventos
+            </span>
+        </a>
+
+        <a href="/admin/eventos" class="dashboard__enlace">
+            <i class="fa-solid fa-users"></i>
+            <span class="dashboard__menu-texto">
+                Registrados
+            </span>
+        </a>
+
+        <a href="/admin/eventos" class="dashboard__enlace">
+            <i class="fa-solid fa-gifts"></i>
+            <span class="dashboard__menu-texto">
+                Registrados
+            </span>
+        </a>
+
+    </nav>
+</aside>
