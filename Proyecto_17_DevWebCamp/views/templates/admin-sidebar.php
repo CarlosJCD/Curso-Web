a757fc41bfa002373465d65dd0c538b8fd7ab41f
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-26 18:31:29 -0600

    Video #697 - Seccion #84
    Aplicando estilos al sidebar del dashboard de admin

--- Proyecto_17_DevWebCamp/views/templates/admin-sidebar.php
+++ Proyecto_17_DevWebCamp/views/templates/admin-sidebar.php
@@ -1,35 +1,35 @@
 <aside class="dashboard__sidebar">
     <nav class="dashboard__menu">
         <a href="/admin/dashboard" class="dashboard__enlace">
-            <i class="fa-solid fa-house"></i>
+            <i class="fa-solid fa-house dashboard__icono"></i>
             <span class="dashboard__menu-texto">
                 Inicio
             </span>
         </a>
 
         <a href="/admin/ponentes" class="dashboard__enlace">
-            <i class="fa-solid fa-user-tie"></i>
+            <i class="fa-solid fa-user-tie dashboard__icono"></i>
             <span class="dashboard__menu-texto">
                 Ponentes
             </span>
         </a>
 
         <a href="/admin/eventos" class="dashboard__enlace">
-            <i class="fa-regular fa-calendar-days"></i>
+            <i class="fa-regular fa-calendar-days dashboard__icono"></i>
             <span class="dashboard__menu-texto">
                 Eventos
             </span>
         </a>
 
         <a href="/admin/eventos" class="dashboard__enlace">
-            <i class="fa-solid fa-users"></i>
+            <i class="fa-solid fa-users dashboard__icono"></i>
             <span class="dashboard__menu-texto">
                 Registrados
             </span>
         </a>
 
         <a href="/admin/eventos" class="dashboard__enlace">
-            <i class="fa-solid fa-gifts"></i>
+            <i class="fa-solid fa-gifts dashboard__icono"></i>
             <span class="dashboard__menu-texto">
                 Registrados
             </span>
