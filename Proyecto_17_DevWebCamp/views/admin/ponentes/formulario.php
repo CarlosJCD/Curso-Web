b88462b60b9d2b818ca1507e728682a9fec10c89
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-26 20:22:13 -0600

    Video #703 - Seccion #85
    Añadiendo campos de tags e imagen de ponente

--- Proyecto_17_DevWebCamp/views/admin/ponentes/formulario.php
+++ Proyecto_17_DevWebCamp/views/admin/ponentes/formulario.php
@@ -11,4 +11,29 @@
         <input type="text" id="apellido" class="formulario__input" name="apellido" placeholder="Apellido Ponente" value="<?php echo $ponente->apellido ?? ''; ?>">
     </div>
 
+    <div class="formulario__campo">
+        <label for="ciudad" class="formulario__label">Ciudad</label>
+        <input type="text" class="formulario__input" id="ciudad" name="ciudad" placeholder="Ciudad Ponente" value="<?php echo $ponente->ciudad ?? ''; ?>">
+    </div>
+
+    <div class="formulario__campo">
+        <label for="pais" class="formulario__label">País</label>
+        <input type="text" class="formulario__input" id="pais" name="pais" placeholder="País Ponente" value="<?php echo $ponente->pais ?? ''; ?>">
+    </div>
+    <div class="formulario__campo">
+        <label for="imagen" class="formulario__label">Imagen</label>
+        <input type="file" class="formulario__input formulario__input--file" id="imagen" name="imagen">
+    </div>
+</fieldset>
+
+<fieldset class="formulario__fieldset">
+    <legend class="formulario__legend">Información Extra</legend>
+
+    <div class="formulario__campo">
+        <label for="tags_input" class="formulario__label">Áreas de Experiencia (separadas por coma)</label>
+        <input type="text" class="formulario__input" id="tags_input" placeholder="Ej. Node.js, PHP, CSS, Laravel, UX / UI">
+
+        <div id="tags" class="formulario__listado"></div>
+        <input type="hidden" name="tags" value="<?php echo $ponente->tags ?? ''; ?>">
+    </div>
 </fieldset>
