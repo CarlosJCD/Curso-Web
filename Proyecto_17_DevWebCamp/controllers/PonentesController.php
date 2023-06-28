0ff492c07ae223d87cf2baa5090e2a2183f40031
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-27 18:23:43 -0600

    Video #708 - Seccion #85
    - Leyendo la imagen del formulario de ponentes.
    - Generando las versiones webp y png de la imagen del ponente.
    - Generando un nuevo hash como nombre de la imagen que se va a guardar.

--- Proyecto_17_DevWebCamp/controllers/PonentesController.php
+++ Proyecto_17_DevWebCamp/controllers/PonentesController.php
@@ -5,6 +5,7 @@ namespace Controllers;
 
 use MVC\Router;
 use Model\Ponente;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class PonentesController
 {
@@ -19,6 +20,22 @@ class PonentesController
     {
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            if (!empty($_FILES['imagen']['tmp_name'])) {
+                $ruta_carpeta_imagenes = '../public/img/speakers';
+
+                if (!is_dir($ruta_carpeta_imagenes)) {
+                    mkdir($ruta_carpeta_imagenes, 0755, true);
+                }
+
+                $imagen_png = Image::make($_FILES['imagen']['tmp_name'])->fit(800, 800)->encode('png', 80);
+                $imagen_webp = Image::make($_FILES['imagen']['tmp_name'])->fit(800, 800)->encode('webp', 80);
+
+                $nombre_imagen = md5(uniqid(rand(), true));
+
+                $_POST['imagen'] = $nombre_imagen;
+            }
+
             $ponente = new Ponente($_POST);
 
             $alertas = $ponente->validar();
