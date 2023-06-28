474881d9f51d8aaf9882916eeb37dff2c92a038a
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-27 22:06:22 -0600

    Video #716 - Seccion #86
    - Validando que se haya puesto una imagen nueva
    - Asignando la imagen nueva en caso de que haya sido puesta en el formulario
    - Almacenando los datos actualizados del ponente en la base de datos

--- Proyecto_17_DevWebCamp/controllers/PonentesController.php
+++ Proyecto_17_DevWebCamp/controllers/PonentesController.php
@@ -73,6 +73,40 @@ class PonentesController
         $redes = json_decode($ponente->redes);
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (!empty($_FILES['imagen']['tmp_name'])) {
+
+                $carpeta_imagenes = '../public/img/speakers';
+
+                // Crear la carpeta si no existe
+                if (!is_dir($carpeta_imagenes)) {
+                    mkdir($carpeta_imagenes, 0755, true);
+                }
+
+                $imagen_png = Image::make($_FILES['imagen']['tmp_name'])->fit(800, 800)->encode('png', 80);
+                // $imagen_webp = Image::make($_FILES['imagen']['tmp_name'])->fit(800, 800)->encode('webp', 80);
+
+                $nombre_imagen = md5(uniqid(rand(), true));
+
+                $_POST['imagen'] = $nombre_imagen;
+            } else {
+                $_POST['imagen'] = $ponente->imagen_actual;
+            }
+
+            $_POST['redes'] = json_encode($_POST['redes'], JSON_UNESCAPED_SLASHES);
+            $ponente->sincronizar($_POST);
+
+            $alertas = $ponente->validar();
+
+            if (empty($alertas)) {
+                if (isset($nombre_imagen)) {
+                    $imagen_png->save($carpeta_imagenes . '/' . $nombre_imagen . ".png");
+                    // $imagen_webp->save($carpeta_imagenes . '/' . $nombre_imagen . ".webp");
+                }
+                $resultado = $ponente->guardar();
+                if ($resultado) {
+                    header('Location: /admin/ponentes');
+                }
+            }
         }
 
 
