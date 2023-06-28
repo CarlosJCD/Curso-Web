0f8c3cdba690af77e20de6d61bd6a6f6a3309a91
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-28 15:14:19 -0600

    Video #723 - Seccion #87
    Creando metodos en la clase de Paginacion para:
     - Obtener el offset de la pagina actual
     - Obtener el total de paginas
     - Obtener la pagina anterior
     - Obtener la pagina siguiente

--- Proyecto_17_DevWebCamp/classes/Paginacion.php
+++ Proyecto_17_DevWebCamp/classes/Paginacion.php
@@ -14,4 +14,23 @@ class Paginacion
         $this->registros_por_pagina = (int) $registros_por_pagina;
         $this->total_registros = (int) $total_registros;
     }
+    public function offset()
+    {
+        return $this->registros_por_pagina * ($this->pagina_actual - 1);
+    }
+
+    public function total_paginas()
+    {
+        return ceil($this->total_registros / $this->registros_por_pagina);
+    }
+
+    public function pagina_anterior()
+    {
+        return ($this->pagina_actual - 1) > 0 ? $this->pagina_actual - 1 : false;
+    }
+
+    public function pagina_siguiente()
+    {
+        return ($this->pagina_actual + 1) > $this->total_paginas() ? $this->pagina_actual + 1 : false;
+    }
 }
