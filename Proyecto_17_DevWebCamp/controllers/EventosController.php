533db911b3c7bb7b020b6b90b4fee1191179dc21
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-29 10:24:48 -0600

    Video #749 - Seccion #89 (INICIO)

--- Proyecto_17_DevWebCamp/controllers/EventosController.php
+++ Proyecto_17_DevWebCamp/controllers/EventosController.php
@@ -5,16 +5,42 @@ namespace Controllers;
 use Model\Dia;
 use Model\Hora;
 use MVC\Router;
-use Model\Categoria;
 use Model\Evento;
+use Model\Categoria;
+use Classes\Paginacion;
 
 class EventosController
 {
     public static function index(Router $router)
     {
+        $paginacion = self::generarPaginacion($_GET['page'], 2);
+
+        $eventos = Evento::paginar($paginacion->registros_por_pagina, $paginacion->offset());
+
         $router->render('admin/eventos/index', [
-            'titulo' => 'Conferencias y workshops'
+            'titulo' => 'Conferencias y workshops',
+            'paginacion' => $paginacion,
+            'eventos' => $eventos
         ]);
+    }
+
+    private static function generarPaginacion($numeroPagina, $registrosPorPagina)
+    {
+        $pagina_actual = filter_var($numeroPagina, FILTER_VALIDATE_INT);
+        if (!$pagina_actual || $pagina_actual < 1) {
+            header('Location: /admin/eventos?page=1');
+            return;
+        }
+
+        $total_registros = Evento::total();
+        $paginacion = new Paginacion($pagina_actual, $registrosPorPagina, $total_registros);
+
+        if ($pagina_actual > $paginacion->total_paginas()) {
+            header('Location: /admin/eventos?page=1');
+            return;
+        }
+
+        return $paginacion;
     }
 
     public static function crear(Router $router)
--- Proyecto_17_DevWebCamp/views/admin/eventos/index.php
+++ Proyecto_17_DevWebCamp/views/admin/eventos/index.php
@@ -6,3 +6,38 @@
         Añadir Evento
     </a>
 </div>
+
+<div class="dashboard__contenedor">
+    <?php if (!empty($eventos)) { ?>
+        <table class="table">
+            <thead class="table__thead">
+                <tr>
+                    <th scope="col" class="table__th">Evento</th>
+                    <th scope="col" class="table__th">Categoria</th>
+                    <th scope="col" class="table__th">Dia y Hora</th>
+                    <th scope="col" class="table__th">Ponente</th>
+                    <th scope="col" class="table__th"></th>
+                </tr>
+            </thead>
+            <tbody class="table__tbody">
+                <?php foreach ($eventos as $evento) {  ?>
+                    <tr class="table__tr">
+                        <td class="table__td">
+                            <?php echo $evento->nombre; ?>
+                        </td>
+
+                        <td class="table__td">
+                            <?php echo $evento->categoria_id; ?>
+                        </td>
+
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+
+
+    <?php } else { ?>
+        <p class="text-center">No Hay Ponentes Aún.</p>
+
+    <?php } ?>
+</div>
