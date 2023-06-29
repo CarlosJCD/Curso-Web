a2343bef93bea59a8ca34048aa55b9cc20b98365
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-29 13:48:57 -0600

    Video #758 - Seccion #90

---
+++ Proyecto_17_DevWebCamp/views/paginas/conferencias.php
@@ -0,0 +1,30 @@
+<main class="agenda">
+    <h2 class="agenda__heading"><?php echo $titulo ?></h2>
+    <p class="agenda__descripcion">Talleres y Conferencias dictados por expertos en desarrollo web</p>
+
+    <div class="eventos">
+        <h3 class="eventos__heading">&lt; Conferencias /></h3>
+        <p class="eventos__fecha">Viernes 5 de Octubre</p>
+
+        <div class="eventos__listado">
+        </div>
+
+        <p class="eventos__fecha">Sabado 6 de Octubre</p>
+
+        <div class="eventos__listado">
+        </div>
+    </div>
+
+    <div class="eventos--workshops">
+        <h3 class="eventos__heading">&lt; Workshops /></h3>
+        <p class="eventos__fecha">Viernes 5 de Octubre</p>
+
+        <div class="eventos__listado">
+        </div>
+
+        <p class="eventos__fecha">Sabado 6 de Octubre</p>
+
+        <div class="eventos__listado">
+        </div>
+    </div>
+</main>
