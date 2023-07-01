72de67df89797b59d65a2cc92da72958a22bf0f5
CarlosJCD <carlos.calderon.d@icloud.com>
2023-07-01 12:42:39 -0600

    Correcciones imagenes y overflow

--- Proyecto_17_DevWebCamp/views/templates/evento.php
+++ Proyecto_17_DevWebCamp/views/templates/evento.php
@@ -7,9 +7,9 @@
         </div>
         <div class="evento__autor-info">
             <picture>
-                <source srcset="/public/img/speakers/<?php echo $evento->ponente->imagen; ?>.webp" type="image/webp">
-                <source srcset="/public/img/speakers/<?php echo $evento->ponente->imagen; ?>.png" type="image/png">
-                <img class="evento__imagen-autor" loading="lazy" width="200" height="300" src="/public/img/speakers/<?php echo $evento->ponente->imagen; ?>.png" alt="Imagen Ponente">
+                <source srcset="/img/speakers/<?php echo $evento->ponente->imagen; ?>.webp" type="image/webp">
+                <source srcset="/img/speakers/<?php echo $evento->ponente->imagen; ?>.png" type="image/png">
+                <img class="evento__imagen-autor" loading="lazy" width="200" height="300" src="/img/speakers/<?php echo $evento->ponente->imagen; ?>.png" alt="Imagen Ponente">
             </picture>
             <p class="evento__autor-nombre"><?php echo $evento->ponente->nombre . " " . $evento->ponente->apellido ?></p>
         </div>
