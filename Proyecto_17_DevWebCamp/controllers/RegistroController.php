856cd1ef43e7764734dc0b6f6e1431e2eb46dfc1
CarlosJCD <carlos.calderon.d@icloud.com>
2023-06-30 21:11:58 -0600

    Video #781 - Seccion #92

--- Proyecto_17_DevWebCamp/controllers/RegistroController.php
+++ Proyecto_17_DevWebCamp/controllers/RegistroController.php
@@ -11,6 +11,11 @@ class RegistroController
 {
     public static function crear(Router $router)
     {
+
+        validarAuth("/");
+
+        self::validarUsuarioYaRegistrado();
+
         $router->render('registro/crear', [
             'titulo' => 'Finalizar Registro'
         ]);
@@ -73,5 +78,14 @@ class RegistroController
         }
 
         return $registro;
+    }
+
+    private static function validarUsuarioYaRegistrado()
+    {
+        $registro = Registro::where('usuario_id', $_SESSION['id']);
+
+        if (isset($registro) && $registro->paquete_id === '3') {
+            header("Location: /boleto?token=" . urlencode($registro->token));
+        }
     }
 }
